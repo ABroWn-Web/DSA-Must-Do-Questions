6fba8f2658f22e0e19ecfd6be61494421cafcd71
ABroWn-Web <harshsharma29k@gmail.com>
2025-03-11 19:41:53 +0530

    Update fizzBuzz.cpp
    The FizzBuzz problem is a classic programming challenge where number from 1 to n are printed, replacing multiples of:
    3 with "Fizz",
    5 with "Buzz",
    both 3 and 5 with "FizzBuzz".
    
    This implementation optimizes the logic to make it more efficient and readable

--- 01_Fizz Buzz/Ideal Solutions/fizzBuzz.cpp
+++ 01_Fizz Buzz/Ideal Solutions/fizzBuzz.cpp
@@ -1,20 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 void fizzBuzz(int n) {
-	// Function to implement Fizz Buzz
-	for (int i = 1; i <= n; i++) {
-		if (i % 3 == 0 && i % 5 != 0)
-			cout << "Fizz" << " ";
-		if (i % 5 == 0 && i % 3 != 0)
-			cout << "Buzz" << " ";
-		if (i % 5 == 0 && i % 3 == 0)
-			cout << "FizzBuzz" << " ";
-		if (i % 5 != 0 && i % 3 != 0)
-			cout << i << " ";
-	}
+    for (int i = 1; i <= n; i++) {
+        string output = "";
+
+        if (i % 3 == 0) output += "Fizz";
+        if (i % 5 == 0) output += "Buzz";
+
+        if (output.empty()) 
+            cout << i;
+        else 
+            cout << output;
+
+        cout << endl;
+    }
 }
+
 int main() {
-	int n = 10;
-	fizzBuzz(n);
-	return 0;
+    int n = 10;
+    fizzBuzz(n);
+    return 0;
 }
